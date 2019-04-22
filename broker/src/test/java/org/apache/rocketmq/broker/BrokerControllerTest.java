26bfc1a74e86ca890f5fd555be60b29e8d3688e5
lifreesh <lifreesh@gmail.com>
2019-04-22 16:59:49 +0800

    NameServer BrokerController

--- broker/src/test/java/org/apache/rocketmq/broker/BrokerControllerTest.java
+++ broker/src/test/java/org/apache/rocketmq/broker/BrokerControllerTest.java
@@ -18,10 +18,16 @@
 package org.apache.rocketmq.broker;
 
 import java.io.File;
+
+import com.sun.corba.se.pept.broker.Broker;
+import org.apache.commons.lang3.time.DateUtils;
 import org.apache.rocketmq.common.BrokerConfig;
+import org.apache.rocketmq.common.MQVersion;
 import org.apache.rocketmq.common.UtilAll;
 import org.apache.rocketmq.remoting.netty.NettyClientConfig;
 import org.apache.rocketmq.remoting.netty.NettyServerConfig;
+import org.apache.rocketmq.remoting.protocol.RemotingCommand;
+import org.apache.rocketmq.store.config.FlushDiskType;
 import org.apache.rocketmq.store.config.MessageStoreConfig;
 import org.junit.After;
 import org.junit.Ignore;
@@ -46,5 +52,31 @@ public class BrokerControllerTest {
     @After
     public void destroy() {
         UtilAll.deleteFile(new File(new MessageStoreConfig().getStorePathRootDir()));
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        System.setProperty(RemotingCommand.REMOTING_VERSION_KEY, Integer.toString(MQVersion.CURRENT_VERSION));
+        final NettyServerConfig nettyServerConfig = new NettyServerConfig();
+        nettyServerConfig.setListenPort(10911);
+        final BrokerConfig brokerConfig = new BrokerConfig();
+        brokerConfig.setBrokerName("broker-a");
+        brokerConfig.setNamesrvAddr("127.0.0.1:9876");
+        final MessageStoreConfig messageStoreConfig = new MessageStoreConfig();
+        messageStoreConfig.setDeleteWhen("04");
+        messageStoreConfig.setFileReservedTime(48);
+        messageStoreConfig.setFlushDiskType(FlushDiskType.ASYNC_FLUSH);
+        messageStoreConfig.setDuplicationEnable(false);
+        BrokerController brokerController = new BrokerController(
+                brokerConfig,
+                nettyServerConfig,
+                new NettyClientConfig(),
+                messageStoreConfig
+        );
+        brokerController.initialize();
+        brokerController.start();
+        System.out.println("测试");
+        Thread.sleep(DateUtils.MILLIS_PER_DAY);
+
     }
 }
